6bf920d7c054f1e4190a064a682424383b468427
unknown <ag444107@gmail.com>
2025-05-12 09:43:34 -0300

    Correcciones del código. Se puso el valor de dolares como constante, se cambió el WHILE por un DO WHILE, se agregó  un default y se agregó también un objeto para que el historial se muestre por consola usando JSON. Le puse una validación para los pesos que ingresa el usuario.

--- app.js
+++ app.js
@@ -1,25 +1,48 @@
-let opt = prompt('Ingrese una opción válida (1-2)\n1. Convertir pesos a dolares\n2. Salir');
+const usd = 1300;
+let opt;
 
-let usd = 1100
+// Función para mostrar el menú de opciones
+// y solicitar la opción al usuario
 
+do {
+    opt = prompt('Ingrese una opción válida (1-2)\n1. Convertir pesos a dólares\n2. Salir');
 
-while (opt == "1") {
-
-
-    switch (opt) { 
+    switch (opt) {
         case '1':
+            
             let pesos = parseFloat(prompt("Ingrese la cantidad de pesos a convertir"));
+            
+            // Validar que la entrada sea un número
+            // y que no sea NaN
+            if (isNaN(pesos)) {
+                alert("Por favor, ingrese un número válido.");
+                break;
+            }
+
             let resultado = pesos / usd;
-            alert("La cantidad de dolares es: " + resultado.toFixed(2));
+
+            // Guardar la conversión en un objeto
+            // y convertirlo a JSON
+            let conversion = {
+                tipo: 'Divisa',
+                Entrada: pesos + ' ARS',
+                resultado: resultado.toFixed(2) + ' USD',
+                fecha: new Date().toLocaleDateString(),
+            };
+
+            // Mostrar el resultado
+            // y los detalles de la conversión
+            alert('La cantidad de dólares es: ' + resultado.toFixed(2));
+            console.log('Detalles de la conversión:', JSON.stringify(conversion, null, 2));
             break;
 
         case '2':
-           
+            alert("Saliendo del programa...");
             break;
-    
+
+        default:
+            alert("Opción no válida. Intente de nuevo.");
+            break;
     }
 
-    opt = prompt('Ingrese una opción válida (1-2)\n1. Convertir pesos a dolares\n2. Salir');
-
-}
-
+} while (opt !== '2');
